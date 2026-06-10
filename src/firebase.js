8df551c91d2c93d2b886ab1e8fbf10319fe7cfb8
Koborlo <kwadwokoboloni@gmail.com>
2026-06-10 17:45:22 +0000

    update app changes

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  apiKey: "AIzaSyCy5gO6PVtTKJA37PcT0j4lQJfuBOUKaYA",
+  authDomain: "schoolmark-gh.firebaseapp.com",
+  projectId: "schoolmark-gh",
+  storageBucket: "schoolmark-gh.firebasestorage.app",
+  messagingSenderId: "504392563457",
+  appId: "1:504392563457:web:e205408ca7f1a7b4c1b900"
 };
 
 const missingFirebaseConfig = Object.entries(firebaseConfig)
